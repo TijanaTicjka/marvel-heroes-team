b232c6e49e9af9c93d915b5ec67d459719d519b0
TijanaTicjka <tijanamilenkovic13@gmail.com>
2023-04-10 21:38:11 +0200

    Worked on rendering SingleCharacter component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,35 +1,10 @@
 import './Layout.css';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-// import { useState } from "react";
-// import { useEffect } from 'react';
 import { Outlet } from 'react-router';
 
 export const Layout = () => {
-//   const [addedCharacters, setAddedCharacters] = useState([]);
-//   const [str, setStr] = useState('');
-//   const [ids, setIds] = useState([]);
 
-//   useEffect(() => {
-//     window.addEventListener('scroll', handleScroll);
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, []);
-
-//   const handleScroll = () => {
-//     const scrollTop = window.pageYOffset;
-//     if (scrollTop === 0) {
-//       document.body.style.overflow = 'hidden';
-//     } else {
-//       document.body.style.overflow = 'auto';
-//     }
-//   };
-
-
-//   const handleSend =(searchTerm) => {
-//     setStr(searchTerm);
-//   }
   return (
     <div className='layout'>
       <Header/>
--- src/components/SingleCharacter/SingleCharacter.js
+++ src/components/SingleCharacter/SingleCharacter.js
@@ -1,20 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import './SingleCharacter.css';
 
 export const SingleCharacter = () => {
-
+  const [hero, setHero] = useState({});
   const { id } = useParams();
+  console.log(hero);
 
   useEffect(() => {
-    fetch(`https://gateway.marvel.com/v1/public/characters/${id}?apikey=28f6359f5b4f28caadd97f5b833f6fe6&`)
+    fetch(`https://gateway.marvel.com/v1/public/characters/${id}?apikey=28f6359f5b4f28caadd97f5b833f6fe6`)
       .then(response => response.json())
       .then(data => {
-        console.log(data.data.results[0]);
+        setHero(data.data.results[0]);
       });
   }, [id]);
 
   return (
-    <div>
+    <div className="character-div">
+        <div className="info-box">
+            <div className="photo">
+                <img src={`${hero.thumbnail?.path}.${hero.thumbnail?.extension}`} alt={hero.name}></img>
+            </div>
+            <div className="description">
+                <h1>{hero.name}</h1>
+                <p>{hero.description}</p>
+            </div>
+        </div>
     </div>
   );
 };
+
